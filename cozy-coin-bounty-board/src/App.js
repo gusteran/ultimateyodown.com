aaf59e8bf1bd7d4d4e35b3a84595055a435fe637
Gus Taylor Teran <ateran@wpi.edu>
2023-12-15 21:46:32 -0700

    Setup react app

---
+++ cozy-coin-bounty-board/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import yo_down_logo from './resources/UltimateYoDownLogo.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={yo_down_logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
